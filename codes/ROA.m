b46cf0a3a08bb650350ddd06e183d5108b2a3b94
pramodkachare <Kachare.pramod1991@gmail.com>
2023-09-15 15:13:00 +0530

    Update docstring & input, output variables

--- codes/ROA.m
+++ codes/ROA.m
@@ -1,12 +1,10 @@
-%RSA Reptile Search Algorithm (RSA)
-% [Best_F,Best_P, conv_curve, CT] = RSA(X, y, No_P, fobj, N_Var, Max_Iter, LB, UB, verbose)
+%ROA Reptile Search Algorithm
+% [Best_F,Best_P, conv_curve, CT] = ROA(X, y, No_P, fobj, N_Var, Max_Iter, LB, UB, verbose)
 % 
-%   Main paper: Abualigah, L., Abd Elaziz, M., Sumari, P., Geem, Z. W., 
-%               & Gandomi, A. H. (2022). 
-%               Reptile Search Algorithm (RSA): A nature-inspired 
-%               meta-heuristic optimizer
-%               Expert Systems with Applications, 191, 116158.
-%               DOI: 10.1016/j.eswa.2021.116158 
+%   Main paper: Jia, H., Peng, X., & Lang, C. (2021). 
+%               Remora optimization algorithm
+%               Expert Systems with Applications, 185, 115665.
+%               DOI: 10.1016/j.eswa.2021.115665 
 % 
 %     [Best_F, Best_P] = RSA(X) applies feature selection on M-by-N matrix X
 %     with N examples and assuming last column as the classification target 
@@ -23,38 +21,37 @@
 % Original Author: Dr. Seyedali Mirjalili
 % Revised by : Pramod H. Kachare (Aug 2023)
 
-function [Score,BestRemora,Convergence]=ROA(Search_Agents,Max_iterations,...
-    Lowerbound,Upperbound,dimensions,objective)
+function [Best_F,Best_Pos,conv_curve, CT]=ROA(X, y, No_P, fobj, N_Var, Max_Iter, LB, UB, verbose)
 tic;       
-BestRemora=zeros(1,dimensions);
-Score=inf; 
-Remora=init(Search_Agents,dimensions,Upperbound,Lowerbound); % Generate initial remora population
+Best_Pos=zeros(1,N_Var);
+Best_F=inf; 
+Remora=init(Search_Agents,N_Var,UB,LB); % Generate initial remora population
 Prevgen{1}=Remora; 
-Convergence=zeros(1,Max_iterations);
-t=0;
-while t<Max_iterations  
+conv_curve=zeros(1,Max_Iter);
+tt=0;
+while tt<Max_Iter  
     
 %% Memory of previous generation
 
-    if t<=1
+    if tt<=1
         PreviousRemora = Prevgen{1};
     else
-        PreviousRemora = Prevgen{t-1};
+        PreviousRemora = Prevgen{tt-1};
     end
     
 % Boundary check
 
     for i=1:size(Remora,1)
-        Flag4Upperbound=Remora(i,:)>Upperbound;
-        Flag4Lowerbound=Remora(i,:)<Lowerbound;
-        Remora(i,:)=(Remora(i,:).*(~(Flag4Upperbound+Flag4Lowerbound)))+Upperbound.*Flag4Upperbound+Lowerbound.*Flag4Lowerbound;
-        fitness=objective(Remora(i,:));
+        Flag4Upperbound=Remora(i,:)>UB;
+        Flag4Lowerbound=Remora(i,:)<LB;
+        Remora(i,:)=(Remora(i,:).*(~(Flag4Upperbound+Flag4Lowerbound)))+UB.*Flag4Upperbound+LB.*Flag4Lowerbound;
+        fitness=fobj(Remora(i,:));
         
  % Evaluate fitness function of search agents   
  
-		if fitness<Score 
-            Score=fitness; 
-            BestRemora=Remora(i,:);
+		if fitness<Best_F 
+            Best_F=fitness; 
+            Best_Pos=Remora(i,:);
         end
         
     end
@@ -66,39 +63,39 @@ while t<Max_iterations
         
   % Calculate the fitness function value of the attempted solution (fitnessAtt)
   
-        fitnessAtt=objective(RemoraAtt);
+        fitnessAtt=fobj(RemoraAtt);
         
   % Calculate the fitness function value of the current solution (fitnessI)
   
-        fitnessI=objective(Remora(j,:));
+        fitnessI=fobj(Remora(j,:));
         
   % Check if the current fitness (fitnessI) is better than the attempted fitness(fitnessAtt)
   % if No, Perform host feeding by equation (9)
   
         if fitnessI>fitnessAtt
-            V = 2*(1-t/Max_iterations);                                                     % Equation (12)
+            V = 2*(1-tt/Max_Iter);                                                     % Equation (12)
             B = 2*V*rand-V;                                                                 % Equation (11)
             C = 0.1;
-            A = B*(Remora(j,:)-C*BestRemora);                                               % Equation (10)
+            A = B*(Remora(j,:)-C*Best_Pos);                                               % Equation (10)
             Remora(j,:)= Remora(j,:)+A;                                                     % Equation (9)
             
   % If yes perform host conversion using equation (1) and (5)       
   
         elseif randi([0 1],1)==0
-            a=-(1+t/Max_iterations);                                                        % Equation (7)
+            a=-(1+tt/Max_Iter);                                                        % Equation (7)
             alpha = rand*(a-1)+1;                                                           % Equation (6)
-            D = abs(BestRemora-Remora(j,:));                                                % Equation (8)
+            D = abs(Best_Pos-Remora(j,:));                                                % Equation (8)
             Remora(j,:) = D*exp(alpha)*cos(2*pi*a)+Remora(j,:);                             % Equation (5)
         else 
             m=randperm(size(Remora,1));
-            Remora(j,:)=BestRemora-((rand*(BestRemora+Remora(m(1),:))/2)-Remora(m(1),:));   % Equation (1)
+            Remora(j,:)=Best_Pos-((rand*(Best_Pos+Remora(m(1),:))/2)-Remora(m(1),:));   % Equation (1)
         end
     end
     
-  t=t+1 ;
-  fprintf('Iteration : %d, best score : %d\n',t,Score)
-  Prevgen{t+1}=Remora; 
-  Convergence(t)=Score;
+  tt=tt+1 ;
+  fprintf('Iteration : %d, best score : %d\n',tt,Best_F)
+  Prevgen{tt+1}=Remora; 
+  conv_curve(tt)=Best_F;
     end   
 end
 
