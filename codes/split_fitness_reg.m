77225d4a319e3633fb8baa43808152e1dc1cba53
pramodkachare <Kachare.pramod1991@gmail.com>
2023-09-17 17:35:43 +0530

    Regression fitness
    SVR based Adjusted R-squared value

---
+++ codes/split_fitness_reg.m
@@ -0,0 +1,19 @@
+function fitness = split_fitness_reg(Pos, X, y, lambda)
+    if nargin < 4   % Default weightage for loss in fitness calculation
+        lambda = 0.99;
+    end
+    
+    f_ind = Pos > 0.5;    % Selected features
+    
+    if sum(f_ind) > 0     % At least 1 feature must be selected
+        reduced_x_train = X(:,f_ind);   % Reduced feature set
+        
+        % Test model for fitness
+        model = fitrsvm(reduced_x_train, y, 'NumNeighbors',5, 'kFold', 5);
+        loss_train = model.Rsquared.Adjusted;     % 5-fold CV loss
+        
+%       fitness = lambda * (1-acc) + (1-lambda) * fraction_selected_features
+        fitness = lambda * loss_train + (1-lambda) * mean(f_ind);
+    else    % If no feature is selected
+        fitness = 1.0;
+    end
