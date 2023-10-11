16491ae9a6bf6c71c863372f86275230ea32b5ee
pramodkachare <Kachare.pramod1991@gmail.com>
2023-10-11 15:13:14 +0530

    Updated fitness arguments
    Adaptive threshold and data arguments to fitness function

--- codes/ALO.m
+++ codes/ALO.m
@@ -62,29 +62,29 @@ end
 timer = tic();
 
 % Initialize the positions of antlions and ants
-antlion_position=initialization(No_P,N_Var,UB,LB);
-ant_position=initialization(No_P,N_Var,UB,LB);
+AL_pos=initialization(No_P,N_Var,UB,LB);
+Ant_pos=initialization(No_P,N_Var,UB,LB);
 
 % Initialize variables to save the position of elite, sorted antlions, 
 % convergence curve, antlions fitness, and ants fitness
-Sorted_antlions=zeros(No_P,N_Var);
+Sorted_AL=zeros(No_P,N_Var);
 
 conv_curve=zeros(1,Max_Iter);
 AL_fit=zeros(1,No_P);
 Ants_fit=zeros(1,No_P);
 
 % Calculate the fitness of initial antlions and sort them
-for i=1:size(antlion_position,1)
-    AL_fit(1,i)=fobj(antlion_position(i,:)); 
+for i=1:size(AL_pos,1)
+    AL_fit(1,i)=fobj(AL_pos(i,:) > (LB+UB)/2, data, target); 
 end
 
 [sorted_antlion_fitness,sorted_indexes]=sort(AL_fit);
     
 for newindex=1:No_P
-     Sorted_antlions(newindex,:)=antlion_position(sorted_indexes(newindex),:);
+     Sorted_AL(newindex,:)=AL_pos(sorted_indexes(newindex),:);
 end
     
-Elt_AL_P=Sorted_antlions(1,:);
+Elt_AL_P=Sorted_AL(1,:);
 Elt_AL_F=sorted_antlion_fitness(1);
 
 % Main loop start from the second iteration since the first iteration 
@@ -93,7 +93,7 @@ tt=2;
 while tt<Max_Iter+1
     
     % This for loop simulate random walks
-    for i=1:size(ant_position,1)
+    for i=1:size(Ant_pos,1)
         % Select ant lions based on their fitness (the better anlion the higher chance of catching ant)
         Rolette_index=RouletteWheelSelection(1./sorted_antlion_fitness);
         if Rolette_index==-1  
@@ -101,46 +101,46 @@ while tt<Max_Iter+1
         end
       
         % RA is the random walk around the selected antlion by rolette wheel
-        RA=Random_walk_around_antlion(N_Var,Max_Iter,LB,UB, Sorted_antlions(Rolette_index,:),tt);
+        RA=Random_walk_around_antlion(N_Var,Max_Iter,LB,UB, Sorted_AL(Rolette_index,:),tt);
         
         % RA is the random walk around the elite (best antlion so far)
         [RE]=Random_walk_around_antlion(N_Var,Max_Iter,LB,UB, Elt_AL_P(1,:),tt);
         
-        ant_position(i,:)= (RA(tt,:)+RE(tt,:))/2; % Equation (2.13) in the paper          
+        Ant_pos(i,:)= (RA(tt,:)+RE(tt,:))/2; % Equation (2.13) in the paper          
     end
     
-    for i=1:size(ant_position,1)  
+    for i=1:size(Ant_pos,1)  
         
         % Boundar checking (bring back the antlions of ants inside search
         % space if they go beyoud the boundaries
-        Flag4ub=ant_position(i,:)>UB;
-        Flag4lb=ant_position(i,:)<LB;
-        ant_position(i,:)=(ant_position(i,:).*(~(Flag4ub+Flag4lb)))+UB.*Flag4ub+LB.*Flag4lb;  
+        Flag4ub=Ant_pos(i,:)>UB;
+        Flag4lb=Ant_pos(i,:)<LB;
+        Ant_pos(i,:)=(Ant_pos(i,:).*(~(Flag4ub+Flag4lb)))+UB.*Flag4ub+LB.*Flag4lb;  
         
-        Ants_fit(1,i)=fobj(ant_position(i,:));        
+        Ants_fit(1,i)=fobj(Ant_pos(i,:) > (LB+UB)/2, data, target);        
        
     end
     
     % Update antlion positions and fitnesses based of the ants (if an ant 
     % becomes fitter than an antlion we assume it was cought by the antlion  
     % and the antlion update goes to its position to build the trap)
-    double_population=[Sorted_antlions;ant_position];
+    double_population=[Sorted_AL;Ant_pos];
     double_fitness=[sorted_antlion_fitness Ants_fit];
         
-    [double_fitness_sorted I]=sort(double_fitness);
+    [double_fitness_sorted, I]=sort(double_fitness);
     double_sorted_population=double_population(I,:);
         
     AL_fit=double_fitness_sorted(1:No_P);
-    Sorted_antlions=double_sorted_population(1:No_P,:);
+    Sorted_AL=double_sorted_population(1:No_P,:);
         
     % Update the position of elite if any antlinons becomes fitter than it
     if AL_fit(1)<Elt_AL_F 
-        Elt_AL_P=Sorted_antlions(1,:);
+        Elt_AL_P=Sorted_AL(1,:);
         Elt_AL_F=AL_fit(1);
     end
       
     % Keep the elite in the population
-    Sorted_antlions(1,:)=Elt_AL_P;
+    Sorted_AL(1,:)=Elt_AL_P;
     AL_fit(1)=Elt_AL_F;
   
     % Update the convergence curve
